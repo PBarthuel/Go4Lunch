1f4f7d701d29eac6bef1ee946017d3d5aca9e7e7
barthuel <paulbarthuel@hotmail.fr>
2020-01-22 14:18:40 +0100

    ajout googlePlaces dans gradle

--- app/src/main/java/paul/barthuel/go4lunch/MainActivity.java
+++ app/src/main/java/paul/barthuel/go4lunch/MainActivity.java
@@ -10,12 +10,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
--- app/src/main/java/paul/barthuel/go4lunch/SearchRestaurantActivity.java
+++ app/src/main/java/paul/barthuel/go4lunch/SearchRestaurantActivity.java
@@ -2,13 +2,13 @@ package paul.barthuel.go4lunch;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SearchRestaurantActivity extends AppCompatActivity {
 
@@ -16,6 +16,7 @@ public class SearchRestaurantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_restaurant);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
