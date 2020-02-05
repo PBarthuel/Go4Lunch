8701a75003e88ce33a4f42e9b1bfff37feb5b107
Poilusse <paulbarthuel@hotmail.fr>
2020-02-05 16:44:58 +0100

    Création d'une interface, GooglePlacesAPI, d'une classe, RetrofitService, pour la gestion des calls pour l'API GooglePlaces et ajout d'un NavigationDrawer

--- app/src/main/java/paul/barthuel/go4lunch/SearchRestaurantActivity.java
+++ app/src/main/java/paul/barthuel/go4lunch/SearchRestaurantActivity.java
@@ -1,13 +1,20 @@
 package paul.barthuel.go4lunch;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import paul.barthuel.go4lunch.ui.list_view.ListViewFragment;
 import paul.barthuel.go4lunch.ui.map_view.LocalisationFragment;
@@ -22,8 +29,16 @@ public class SearchRestaurantActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
+        Toolbar toolbar = findViewById(R.id.search_restaurant_toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.search_restaurant_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         if(savedInstanceState == null) {
-            displayedfragment(0);
+            displayedFragment(0);
         }
 
         navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -31,13 +46,13 @@ public class SearchRestaurantActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
                     case R.id.navigation_localisation:
-                        displayedfragment(0);
+                        displayedFragment(0);
                         break;
                     case R.id.navigation_list_view:
-                        displayedfragment(1);
+                        displayedFragment(1);
                         break;
                     case R.id.navigation_workmates:
-                        displayedfragment(2);
+                        displayedFragment(2);
                         break;
                 }
                 return true;
@@ -45,7 +60,7 @@ public class SearchRestaurantActivity extends AppCompatActivity {
         });
     }
 
-    private void displayedfragment(int fragmentNumber) {
+    private void displayedFragment(int fragmentNumber) {
         Fragment fragment;
         switch (fragmentNumber) {
             case 0:
@@ -64,6 +79,38 @@ public class SearchRestaurantActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_activity_search) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.search_restaurant_drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 }
---
+++ app/src/main/java/paul/barthuel/go4lunch/data/retrofit/GooglePlacesAPI.java
@@ -0,0 +1,8 @@
+package paul.barthuel.go4lunch.data.retrofit;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GooglePlacesAPI {
+
+}
