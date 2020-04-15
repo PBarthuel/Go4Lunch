5f4dfc4030946f2968183320acdeb01280d58625
barthuel <paulbarthuel@hotmail.fr>
2020-04-15 12:22:44 +0200

    modification du layout de restaurant_detail_fragment et création de content_scrolling_restaurant_detail, et affichage des données via le restaurantDetailFragment

--- app/src/main/java/paul/barthuel/go4lunch/ui/restaurant_detail/RestaurantDetailFragment.java
+++ app/src/main/java/paul/barthuel/go4lunch/ui/restaurant_detail/RestaurantDetailFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.bumptech.glide.Glide;
 
 import paul.barthuel.go4lunch.R;
 import paul.barthuel.go4lunch.injections.ViewModelFactory;
@@ -29,24 +33,32 @@ public class RestaurantDetailFragment extends Fragment {
 
         return fragment;
     }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = new ViewModelProvider(this, ViewModelFactory.getInstance()).get(RestaurantDetailViewModel.class);
+
+    }
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.restaurant_detail_fragment, container, false);
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.restaurant_detail_fragment, container, false);
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this, ViewModelFactory.getInstance()).get(RestaurantDetailViewModel.class);
+        ImageView imageView = view.findViewById(R.id.restaurant_detail_iv);
+        TextView textViewTitle = view.findViewById(R.id.content_scrolling_restaurant_detail_title_tv);
+        TextView textViewAddress = view.findViewById(R.id.content_scrolling_restaurant_detail_address_tv);
+
         mViewModel.init(getArguments().getString(KEY_ID));
         mViewModel.getLiveDataResultDetail().observe(getViewLifecycleOwner(), new Observer<RestaurantDetailInfo>() {
             @Override
             public void onChanged(RestaurantDetailInfo restaurantDetailInfo) {
-
+                Glide.with(imageView).load(restaurantDetailInfo.getImage()).into(imageView);
+                textViewTitle.setText(restaurantDetailInfo.getName());
+                textViewAddress.setText(restaurantDetailInfo.getAddress());
             }
         });
+
+        return view;
     }
 }
